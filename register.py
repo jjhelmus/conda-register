6f2384ff7109f8fcca0cb6c6ef6d528fc02df81e
Jonathan Helmus <jjhelmus@gmail.com>
2018-02-17 18:35:13 -0600

    refactoring 2

--- register.py
+++ register.py
@@ -10,40 +10,51 @@ except:
     from pip._vendor.distlib.database import DistributionPath
 
 from conda.models.prefix_record import PrefixRecord
+from conda.core.link import UnlinkLinkTransaction
+from conda.common.path import get_python_site_packages_short_path
 try:
     from conda.core.prefix_data import PrefixData
 except:
     from conda.core.linked_data import PrefixData
 
-
 DEFAULT_BUILD_NUMBER = 0
 DEFAULT_BUILD_STR = 'frompip_0'
 
 
-# TODO set these from the command line
-pip_pkg_name = 'imagesize'
-target_prefix = '/home/jhelmus/anaconda3/envs/pip_test'
-sp_short_path = 'lib/python3.6/site-packages'
-sp_full_path = os.path.join(target_prefix, sp_short_path)
+def register_dist(dist_name, target_prefix):
+    """ register a distribution with conda. """
 
-# find package details using distlib
-dist_path = DistributionPath([sp_full_path, ], include_egg=True)
-dist = dist_path.get_distribution(pip_pkg_name)
+    # build path to site-packages directory
+    get_python_version = UnlinkLinkTransaction.get_python_version
+    python_ver = get_python_version(target_prefix, [], [])
+    sp_short_path = get_python_site_packages_short_path(python_ver)
+    sp_full_path = os.path.join(target_prefix, sp_short_path)
 
-# create a conda PrefixRecord
-files = [os.path.join(sp_short_path, file_path)
-         for file_path, file_hash, file_size in dist.list_installed_files()]
-prefix_record = PrefixRecord.from_objects(
-    name=dist.name,
-    version=dist.version,
-    files=files,
-    build=DEFAULT_BUILD_STR,
-    build_number=DEFAULT_BUILD_NUMBER,
-)
-# TODO: conda currently checks that prefix_record.fn ends with .tar.bz2.
-# This check should be supressed for prefix record entries which are not
-# derived from tarballs so that prefix_record.fn can be set to None.
-prefix_record.fn = prefix_record.fn + '.tar.bz2'
+    # find package details using distlib
+    dist_path = DistributionPath([sp_full_path, ], include_egg=True)
+    dist = dist_path.get_distribution(dist_name)
 
-print("creating linked package record for %s." % pip_pkg_name)
-PrefixData(target_prefix).insert(prefix_record)
+    # create a conda PrefixRecord
+    files = [os.path.join(sp_short_path, file_path)
+             for file_path, file_hash, file_size in dist.list_installed_files()]
+    prefix_record = PrefixRecord.from_objects(
+        name=dist.name,
+        version=dist.version,
+        files=files,
+        build=DEFAULT_BUILD_STR,
+        build_number=DEFAULT_BUILD_NUMBER,
+    )
+    # TODO: conda currently checks that prefix_record.fn ends with .tar.bz2.
+    # This check should be supressed for prefix record entries which are not
+    # derived from tarballs so that prefix_record.fn can be set to None.
+    prefix_record.fn = prefix_record.fn + '.tar.bz2'
+
+    print("creating linked package record for %s." % dist_name)
+    PrefixData(target_prefix).insert(prefix_record)
+
+
+if __name__ == "__main__":
+    # TODO set these from the command line
+    dist_name = 'imagesize'
+    target_prefix = '/home/jhelmus/anaconda3/envs/pip_test'
+    register_dist(dist_name, target_prefix)
